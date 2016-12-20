e7d10dbaf372eb37b3f413429dcd52d499a64bac
Yang Yang <yangyang2010@u.northwestern.edu>
2016-12-20 16:22:33 -0600

    update the display_results.m in co_susceptible_groups

--- co_susceptible_groups/display_results.m
+++ co_susceptible_groups/display_results.m
@@ -1,8 +1,10 @@
 function display_results(CoGroup, C)
 % display the number of size of co-susceptible groups
-disp(strcat('Total number of co-susceptible groups:',num2str(size(CoGroup,1))));
+ix = ( sum(CoGroup,2) >= 3 );
+disp(strcat('Total number of co-susceptible groups:',...
+    num2str(size(CoGroup(ix,:),1))));
 disp('The size of groups are:');
-disp(num2str(sum(CoGroup,2)'));
+disp(num2str(sum(CoGroup(ix,:),2)'));
 
 % reindex the component by co-susceptible groups
 I = zeros(1,size(CoGroup,2));
@@ -16,8 +18,12 @@ end
 figure;
 colormap('gray');
 subplot(1,2,1);
-imagesc(C);
+imagesc(C, [-0.2 1]);
+axis square
+colorbar
 title('Correlation matrix before re-index');
 subplot(1,2,2);
-imagesc(C(I,I));
+imagesc(C(I,I), [-0.2 1]);
+axis square
+colorbar
 title('Correlation matrix after re-index');
